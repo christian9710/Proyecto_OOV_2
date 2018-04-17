dfb79db5bd513f1e530a327e3e526d1b099e41e6
Brandon Fernandez <Brandon Fernandez@DESKTOP-88OKNKP>
2018-04-17 11:02:23 -0600

    Cn

--- ProyectoFinalOO_V1/src/src/conection/Conector.java
+++ ProyectoFinalOO_V1/src/src/conection/Conector.java
@@ -15,31 +15,12 @@ public class Conector{
 	//atributo de la clase	
 	private static AccesoBD conectorBD = null;
 	
-	/**
-	 *M�todo est�tico que devuelve el 
-	 *objeto AccesoBD para que sea utilizado
-	 *por las clases
-	 *@return objeto del tipo AccesoBD del paquete 
-	 *CapaAccesoDatos
-     * @throws java.sql.SQLException
-	 */
-	 
-//	public static AccesoBD getConector() throws 
-//	java.sql.SQLException,Exception{
-//		if (conectorBD == null){			
-////			conectorBD = new AccesoBD("sun.jdbc.odbc.JdbcOdbcDriver","jdbc:odbc:BDCxC","sa","jass2002");
-//			conectorBD = new AccesoBD("sun.jdbc.odbc.JdbcOdbcDriver","jdbc:odbc:BDCxCAccess","","");
-//		}
-//		return conectorBD;
-//	}
-//	
 	
 	public static AccesoBD getConector() throws java.sql.SQLException,Exception{
 		if(conectorBD==null){
                         String driver="com.mysql.jdbc.Driver";
-                        //String conexion="jdbc:mysql://localhost/softwarecelestial?" + "user=root&password=root";
-                        String conexion="jdbc:mysql://localhost/prueba?" + "user=root&password=root";
-			conectorBD = new AccesoBD("com.mysql.jdbc.Driver","jdbc:mysql://localhost/softwarecelestial?" + "user=root&password=root");
+                        String conexion="jdbc:mysql://localhost/prueba?" + "user=root&password=";
+			conectorBD = new AccesoBD("com.mysql.jdbc.Driver","jdbc:mysql://localhost/softwareCelestial?" + "user=root&password=");
 		}
 		return conectorBD;
 	}
