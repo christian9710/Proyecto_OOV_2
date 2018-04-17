4151b83796fb5e68a2fbda477dffbfca160ecfd7
Brandon Fernandez <Brandon Fernandez@DESKTOP-88OKNKP>
2018-04-17 14:26:31 -0600

    MM

---
+++ ProyectoFinalOO_V1/src/src/proyecto/logic/multiMejora.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.proyecto.logic;
+
+import src.conection.Conector;
+
+/**
+ *
+ * @author unicomer
+ */
+public class multiMejora {
+
+    public Mejora agregarMejora(String descripcion) throws Exception {
+
+        String sql;
+        Mejora miMejora = null;
+
+        sql = "INSERT INTO caracteriscamejora"
+                + "(descripcion)"
+                + "VALUES( '" + descripcion + "')";
+
+        try {
+
+            Conector.getConector().ejecutarSQL(sql);
+            miMejora = new Mejora(descripcion);
+        } catch (Exception ex) {
+
+            throw new Exception("Se agregó correctamente");
+
+        }
+
+        return miMejora;
+    }
+}
