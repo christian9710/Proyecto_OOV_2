af8203af7c79d72e2bcbb115aac14fc533c6c2ba
Brandon Fernandez <Brandon Fernandez@DESKTOP-88OKNKP>
2018-04-17 14:07:53 -0600

    MI

--- ProyectoFinalOO_V1/src/src/proyecto/logic/MultiIntalacion.java
+++ ProyectoFinalOO_V1/src/src/proyecto/logic/MultiIntalacion.java
@@ -1,11 +1,10 @@
-
 package src.proyecto.logic;
 
 import src.conection.Conector;
 
 public class MultiIntalacion {
 
-    public Instalacion insertInstalacion(String estado, String fecha,String hora) throws
+    public Instalacion insertInstalacion(String estado, String fecha, String hora) throws
             java.sql.SQLException, Exception {
         Instalacion insta = null;
         String sql;
@@ -16,13 +15,75 @@ public class MultiIntalacion {
         try {
 
             Conector.getConector().ejecutarSQL(sql);
-            insta=new Instalacion(estado,fecha,hora);
+            insta = new Instalacion(estado, fecha, hora);
 
             //con.close();
         } catch (Exception ex) {
             throw new Exception("No se ha logrado registar la instalacion, contacte al departamento de IT");
         }
         return insta;
+    }
+
+    public Instalacion buscar(String estado) throws
+            java.sql.SQLException, Exception {
+        Instalacion insta = null;
+        java.sql.ResultSet rs;
+        String sql;
+        sql = "SELECT nombre,logo,descripcion"
+                + "FROM Instalacion "
+                + "WHERE estado='" + estado + "';";
+        rs = Conector.getConector().ejecutarSQL(sql, true);
+        if (rs.next()) {
+            insta = new Instalacion(
+                    rs.getString("estado"),
+                    rs.getString("fecha"),
+                    rs.getString("hora"));
+
+        } else {
+            throw new Exception("La instalacion no se ha registrado");
+        }
+        rs.close();
+        return insta;
+    }
+
+    public void borrar(Instalacion miInstala) throws
+            java.sql.SQLException, Exception {
+        java.sql.ResultSet rs;
+        String sql;
+        sql = "DELETE FROM Instalacion "
+                + "WHERE estado='" + miInstala.getEstado() + "';";
+        try {
+            Conector.getConector().ejecutarSQL(sql);
+        } catch (Exception e) {
+            throw new Exception("No se ha realizado la limpieza de la tabla");
+        }
+    }
+
+    public void actualizarInstalacion(Instalacion insta) throws
+            java.sql.SQLException, Exception {
+        String sql;
+        sql = "UPDATE Instalacion "
+                + "SET estado=" + insta.getEstado() + ", "
+                + "fecha='" + insta.getFecha() + "', "
+                + "hora='" + insta.getHora() + "', "
+                + "WHERE estado='" + insta.getEstado() + "';";
+
+        try {
+            Conector.getConector().ejecutarSQL(sql);
+        } catch (Exception e) {
+            throw new Exception("No se logro actualizar.");
+        }
+    }
+
+    public void listarInstalacion() throws Exception {
+        Instalacion insta = null;
+        java.sql.ResultSet rs;
+        String sql;
+        sql = "SELECT estado,fecha,hora"
+                + "FROM Instalacion " + "';";
+
+        rs = Conector.getConector().ejecutarSQL(sql, true);
+
     }
 
 }
